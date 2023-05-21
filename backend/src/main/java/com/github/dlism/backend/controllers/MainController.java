497d44ca8a367e59176bbf07fa8753ba2d92c4f5
Javohir <ismoilov92@gmail.com>
2023-05-22 00:53:16 +0300

    registration user

--- backend/src/main/java/com/github/dlism/backend/controllers/MainController.java
+++ backend/src/main/java/com/github/dlism/backend/controllers/MainController.java
@@ -1,14 +1,40 @@
 package com.github.dlism.backend.controllers;
 
+import com.github.dlism.backend.models.User;
+import com.github.dlism.backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("")
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(Model model) {
+        model.addAttribute("registrationForm", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("registrationForm") User user, Model model) {
+
+        if (!userService.createUser(user)) {
+            model.addAttribute("userExists", "Пользовател уже существует!");
+            return "registration";
+        }
+
+        return "redirect:login";
     }
 }
