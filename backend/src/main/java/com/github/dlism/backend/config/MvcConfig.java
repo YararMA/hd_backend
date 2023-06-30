36a2bb8617ad09e0111977b0fed4cc8c13b81f82
devYarar <ismoilov92@gmail.com>
2023-06-30 17:31:47 +0300

    chore(backend): add custom login form

--- backend/src/main/java/com/github/dlism/backend/config/MvcConfig.java
+++ backend/src/main/java/com/github/dlism/backend/config/MvcConfig.java
@@ -3,10 +3,14 @@ package com.github.dlism.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("forms/login");
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
