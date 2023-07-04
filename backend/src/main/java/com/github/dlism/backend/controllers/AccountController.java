9e5e8ee2a5b217d3c2d01682f813146bde79e1b0
devYarar <ismoilov92@gmail.com>
2023-07-04 17:41:50 +0300

    feature(backend): change password

--- backend/src/main/java/com/github/dlism/backend/controllers/AccountController.java
+++ backend/src/main/java/com/github/dlism/backend/controllers/AccountController.java
@@ -1,7 +1,9 @@
 package com.github.dlism.backend.controllers;
 
+import com.github.dlism.backend.dto.user.UserUpdatePasswordDto;
 import com.github.dlism.backend.dto.user.UserUpdateProfileDto;
 import com.github.dlism.backend.exceptions.DuplicateRecordException;
+import com.github.dlism.backend.exceptions.UpdateException;
 import com.github.dlism.backend.models.User;
 import com.github.dlism.backend.services.UserService;
 import jakarta.validation.Valid;
@@ -32,7 +34,6 @@ public class AccountController {
 
     @GetMapping("/edit")
     public String editPage(@AuthenticationPrincipal User user, Model model) {
-        System.out.println(userService.getById(user.getId()));
         model.addAttribute("user", userService.getById(user.getId()));
         return "account/forms/editUserProfile";
     }
@@ -55,5 +56,29 @@ public class AccountController {
         }
 
         return "account/forms/editUserProfile";
+    }
+
+    @GetMapping("/password")
+    public String changePassword(Model model) {
+        model.addAttribute("user", new UserUpdatePasswordDto());
+        return "account/forms/changePassword";
+    }
+
+    @PostMapping("/password")
+    public String changePassword(@AuthenticationPrincipal User user,
+                                 @Valid @ModelAttribute("user") UserUpdatePasswordDto userUpdatePasswordDto,
+                                 BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "account/forms/changePassword";
+        }
+
+        try {
+            userService.changePassword(user, userUpdatePasswordDto);
+            model.addAttribute("user", new UserUpdatePasswordDto());
+            model.addAttribute("message", "Пароль изменен!");
+        } catch (IllegalArgumentException | UpdateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+        return "account/forms/changePassword";
     }
 }
